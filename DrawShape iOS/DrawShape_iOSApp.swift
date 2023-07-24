fd65949d1c3b2e3ad9ecab17cfe9eada3be2c222
rezaulislamtarek <mrezaulislamtarek.t@gmail.com>
2023-07-24 18:46:29 +0600

    Initial Commit

---
+++ DrawShape iOS/DrawShape_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawShape_iOSApp.swift
+//  DrawShape iOS
+//
+//  Created by Rezaul Islam on 24/7/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawShape_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
